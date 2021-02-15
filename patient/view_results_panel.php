d85142817c38a0c5fd4c5679be024ee2bf3eba64
Anthony Byrne <anthonyrb@pm.me>
2021-02-15 17:44:20 +0000

    Added functionality to create full table from database data

--- patient/view_results_panel.php
+++ patient/view_results_panel.php
@@ -1,35 +1,65 @@
+<style>
+#table-container
+{
+    height: 350px;
+    overflow-x: hidden; 
+    overflow-y: auto;
+}
+
+.tdstyle
+{
+    padding-left: 100px;
+    padding-bottom: 20px;
+    text-align: center;
+}
+</style>
+
 <h1 style="font-size: 2rem;">Past test results</h1>
 <br>
-<article>
+<article id="table-container">
 <?php
-
 include '../db.inc.php'; 
-$sql = "SELECT testSlotID, result, iv FROM PastTestResults";
+$sql = "SELECT testSlotID, result, iv FROM PastTestResults WHERE patientID=$_SESSION[patientID]";
 $result = $con->query($sql);
 mysqli_close($con);
 
+include 'scheduleTests.php';
+
 if ($result->num_rows > 0) 
 {
-    echo '<table><tr><th>TestSlotID</th><th>Result</th></tr>';
+    echo "<table><tr><th class='tdstyle'>Date</th><th class='tdstyle'>Time</th><th class='tdstyle'>Result</th></tr>";
     while($row = $result->fetch_assoc()) {
         $testSlotID = $row['testSlotID'];
         $testResults = $row['result'];
         $iv = hex2bin($row['iv']);
         //$unencrypted_content = openssl_decrypt($content, $cipher, $key, OPENSSL_RAW_DATA, $iv);
 
-        //print table
-        if ($testResults == 'P')
+        include '../db.inc.php'; 
+        $sql = "SELECT Date, Time FROM TestSlot WHERE testSlotID=$testSlotID";
+        $testSlotResult = $con->query($sql);
+        mysqli_close($con);
+
+        if ($testSlotResult->num_rows > 0) 
         {
-            echo "<tr><td>$testSlotID</td><td style='color:red;'><strong>COVID detected</strong></td></tr>";
-        }
-        else if ($testResults == 'N')
-        {
-            echo "<tr><td>$testSlotID</td><td style='color:green;'><strong>COVID Not detected</strong></td></tr>";
-        }
-        else
-        {
-            echo "<tr><td>$testSlotID</td><td><em>PENDING</em></td></tr>";
-        }
+            while($testSlotRow = $testSlotResult->fetch_assoc()) {
+                $testslotDate = $testSlotRow['Date'];
+                $testslotTime = convertIntToTime($testSlotRow['Time']);
+
+                //print table rows
+                if ($testResults == 'P')
+                {
+                    echo "<tr><td class='tdstyle'>$testslotDate</td><td class='tdstyle'>$testslotTime</td><td class='tdstyle' style='color:red;'><strong>COVID detected</strong></td></tr>";
+                }
+                else if ($testResults == 'N')
+                {
+                    echo "<tr><td class='tdstyle'>$testslotDate</td><td class='tdstyle'>$testslotTime</td><td class='tdstyle' style='color:green;'><strong>COVID Not detected</strong></td></tr>";
+                }
+                else
+                {
+                    echo "<tr><td class='tdstyle'>$testslotDate</td><td class='tdstyle'>$testslotTime</td><td class='tdstyle' class='tdstyle'><em>PENDING</em></td></tr>";
+                }
+            }
+        } 
     }
     echo '</table>';
 } 
